b16fddbc9109440f714f43bcc9b7a88077b962eb
Kha Vo Nguyen Anh <kha.vo@orientsoftware.com>
2026-03-14 12:01:36 +0700

    Translate questions to Vietnamese

--- SocOps/Data/Questions.cs
+++ SocOps/Data/Questions.cs
@@ -2,33 +2,33 @@ namespace SocOps.Data;
 
 public static class Questions
 {
-    public const string FREE_SPACE = "FREE SPACE";
+    public const string FREE_SPACE = "CHỖ TRỐNG";
 
     public static readonly List<string> QuestionsList = new()
     {
-        "bikes to work",
-        "has lived in another country",
-        "has a pet",
-        "prefers tea over coffee",
-        "plays an instrument",
-        "speaks more than 2 languages",
-        "has run a marathon",
-        "was born in a different state",
-        "has met a celebrity",
-        "can juggle",
-        "has been skydiving",
-        "loves cooking",
-        "has a garden",
-        "has traveled to Asia",
-        "is left-handed",
-        "has a twin",
-        "plays video games",
-        "does yoga",
-        "has a hidden talent",
-        "loves spicy food",
-        "has been on TV",
-        "collects something unique",
-        "has read a book this month",
-        "knows sign language"
+        "đi xe đạp tới công việc",
+        "đã từng sống ở một quốc gia khác",
+        "có một thú cưng",
+        "thích trà hơn cà phê",
+        "chơi nhạc cụ",
+        "nói được hơn 2 ngôn ngữ",
+        "đã chạy marathon",
+        "sinh ra ở một tiểu bang khác",
+        "đã gặp một người nổi tiếng",
+        "có thể làm trò ảo thuật bằng trái bóng",
+        "đã từng nhảy dù",
+        "yêu thích nấu ăn",
+        "có một khu vườn",
+        "đã từng du lịch tới Châu Á",
+        "thuận tay trái",
+        "có một anh chị em sinh đôi",
+        "chơi video games",
+        "tập yoga",
+        "có một tài năng ẩn giấu",
+        "yêu thích đồ ăn cay",
+        "đã từng xuất hiện trên TV",
+        "sưu tập những thứ độc đáo",
+        "đã đọc một cuốn sách tháng này",
+        "biết ngôn ngữ ký hiệu"
     };
 }
